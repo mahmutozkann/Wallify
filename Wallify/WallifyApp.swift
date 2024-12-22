f82261de743fc0f75abcc2939fcad088635fcf69
Mahmut <sefades@icloud.com>
2024-12-22 12:50:22 +0300

    Initial Commit

---
+++ Wallify/WallifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallifyApp.swift
+//  Wallify
+//
+//  Created by Mahmut Özkan on 22.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct WallifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
